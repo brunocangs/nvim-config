17ea54d2acf7228e966c843dfd809f0f4c348649
Bruno Cangussu <bruno.cangussu@hotmail.com>
2023-11-19 11:43:15 -0300

    Added more plugins, harpoon tailwind etc

--- lua/brunocangs/remaps.lua
+++ lua/brunocangs/remaps.lua
@@ -9,5 +9,3 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
---
+++ lua/plugins/tailwind.lua
@@ -0,0 +1,3 @@
+return {
+'yaegassy/coc-tailwindcss3'
+}
