70ea6fb3e355ad1ac2ca565572e8113464404fe2
Bruno Cangussu <bruno.cangussu@hotmail.com>
2024-01-04 18:33:15 -0300

    Added abolish and more harpoons

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -20,7 +20,7 @@ local on_attach = function(_, bufnr)
 
   nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+  nmap('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
   nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
@@ -58,7 +58,7 @@ require('mason-lspconfig').setup()
 --  define the property 'filetypes' to the map in question.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
   sqlls = {},
@@ -98,6 +98,8 @@ mason_lspconfig.setup_handlers {
           arguments = { vim.api.nvim_buf_get_name(0) },
           title = ""
         })
+      end)
+      vim.keymap.set("n", "<leader>ef", function()
         vim.cmd.EslintFixAll()
       end)
     end
--- lua/brunocangs/sets.lua
+++ lua/brunocangs/sets.lua
@@ -5,25 +5,26 @@ vim.opt.relativenumber = true
 vim.opt.smartindent = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 
 vim.opt.updatetime = 50
-
-vim.g.mapleader = " "
 
 vim.o.clipboard = 'unnamedplus'
 vim.o.breakindent = true
