547fe329a2d3deb403585ac5742827ecd1d568e9
Bruno Cangussu <bruno.cangussu@hotmail.com>
2024-01-09 18:55:52 -0300

    Improved trouble command

--- after/plugin/trouble.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle("document_diagnostics") end)
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,4 +1,17 @@
 return {
- "folke/trouble.nvim",
- dependencies = { "nvim-tree/nvim-web-devicons" }
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    vim.keymap.set("n", "<leader>xn", function()
+      local trouble = require("trouble")
+      trouble.open("workspace_diagnostics")
+      trouble.next({ skip_groups = true, jump = true })
+    end)
+    vim.keymap.set("n", "<leader>xp", function()
+      local trouble = require("trouble")
+      trouble.open("workspace_diagnostics")
+      trouble.previous({ skip_groups = true, jump = true })
+    end)
+    vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle("workspace_diagnostics") end)
+  end
 }
