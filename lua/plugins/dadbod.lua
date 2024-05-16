d6238e8cf9c96d9db702aa942c199e85e3ff7d07
Bruno Cangussu <bruno.cangussu@hotmail.com>
2024-05-16 13:00:12 -0300

    Added sick dadbod keybind to find window

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -1,5 +1,31 @@
 return {
   "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-completion",
+  {
+    "kristijanhusak/vim-dadbod-completion",
+    config = function()
+      vim.keymap.set("n", "<leader>db", function()
+        local winnr = -1
+        for _, tab in pairs(vim.api.nvim_list_tabpages()) do
+          for _, win in pairs(vim.api.nvim_tabpage_list_wins(tab)) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            if string.match(vim.api.nvim_buf_get_name(buf), "dbui$") then
+              winnr = win
+            end
+          end
+        end
+        if winnr ~= -1 then
+          vim.api.nvim_set_current_win(winnr)
+        else
+          vim.cmd("tabnew")
+          vim.cmd("DBUI")
+        end
+      end)
+    end
+  },
+
   "kristijanhusak/vim-dadbod-ui"
 }
+
+
+
+-- end
