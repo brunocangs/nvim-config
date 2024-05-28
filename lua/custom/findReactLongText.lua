50a4b15cae6142bc08467ad598a74e6b910091d4
Bruno Cangussu <bruno.cangussu@hotmail.com>
2024-05-28 16:02:21 -0300

    Updated to account for placeholders

--- lua/custom/findReactLongText.lua
+++ lua/custom/findReactLongText.lua
@@ -24,24 +24,40 @@ local function find_long_jsx_text(bufnr, opts)
   -- Parse the buffer
   local root = parser:parse()[1]:root()
 
-  local query = vim.treesitter.query.parse('tsx', [[
+  local looseStringsQuery = vim.treesitter.query.parse('tsx', [[
 ((jsx_text) @text (#offset! @text))
 ]])
   local lines_with_numbers = {}
   local filename = vim.api.nvim_buf_get_name(bufnr)
   -- Find JSX text nodes with at least 3 characters
-  for _, match, metadata in query:iter_matches(root, bufnr) do
+  for _, match, metadata in looseStringsQuery:iter_matches(root, bufnr) do
     local node = match[1]
-    local text = ts_utils.get_node_text(node, bufnr)[1]
-    if #text >= 3 then
+    local text = vim.treesitter.get_node_text(node, bufnr)
+    if text ~= nil and #text >= 3 then
       table.insert(lines_with_numbers, {
         lnum = metadata[1].range[1] + 1,
         bufnr = opts.bufnr,
         filename = filename,
-        text = filename,
+        text = text,
       })
     end
   end
+  local stringPlaceholders = vim.treesitter.query.parse('tsx', [[
+(jsx_attribute
+  (property_identifier) @property (#eq? @property "placeholder")
+  (_) @inside (#match? @inside "^\"") (#offset! @inside)
+  )
+]])
+  for _, match, metadata in stringPlaceholders:iter_matches(root, bufnr) do
+    local textNode = match[2]
+    table.insert(lines_with_numbers, {
+      lnum = metadata[2].range[1] + 1,
+      bufnr = opts.bufnr,
+      filename = filename,
+      text = "Placeholder: " .. vim.treesitter.get_node_text(textNode, bufnr),
+    })
+  end
+
   if #lines_with_numbers > 0 then
     pickers.new(opts, {
       prompt_title = "Large text",
